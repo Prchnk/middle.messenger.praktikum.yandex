983db6bedf53954919ccfe5aa5375cd2dd12131b
prchnk <job-eng@yandex.ru>
2022-12-02 22:53:34 +0300

    test

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -5,6 +5,8 @@ export function getTargetElement(event: Event): HTMLElement {
 }
 
 export function querySelector(selectors: string, parentNode: Element | null = null) {
+	console.log(1);
+
 	let nodeOrNull = null;
 
 	if (parentNode === null) {
