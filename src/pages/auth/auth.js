b918bc2e20925042c69739c9311f625ce634eaf8
prchnk <job-eng@yandex.ru>
2022-12-04 12:34:35 +0300

    настроен линтер

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,15 +1,14 @@
 import AuthTemplate from './auth.hbs';
 
-
 function renderHbs() {
-	const data = {
-		title: 'Авторизация пользователя',
-		list: [
-			{name: 'login', label: 'Логин', type: 'text'},
-			{name: 'password', label: 'Логин', type: 'password'},
-		],
-	};
-	document.querySelector('#output').innerHTML = AuthTemplate(data);
+  const data = {
+    title: 'Авторизация пользователя',
+    list: [
+      { name: 'login', label: 'Логин', type: 'text' },
+      { name: 'password', label: 'Логин', type: 'password' },
+    ],
+  };
+  document.querySelector('#output').innerHTML = AuthTemplate(data);
 }
 
-renderHbs()
+renderHbs();
--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')));
 app.listen(port, () => {
-	console.log(`server listening on http://localhost:${port}`)
-})
+  console.log(`server listening on http://localhost:${port}`);
+});
